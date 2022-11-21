1b1581568fdbced484bd69b8316c270e44c4aa1f
Issac Cabral <issaccabrall@gmail.com>
2022-11-21 20:45:01 -0300

    test: ensure SignUpController return 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -154,12 +154,29 @@ describe('SignUp Controller', () => {
     expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com")
   })
 
-  // Vamos criar o mesmo teste acima de uma maneira diferente. Ao invés de utilizarmos
-  // uma factory para  o EmailValidator e estarmos mocando, utilizamos o método
+  // Ao invés de utilizarmos uma factory para  o EmailValidator e estarmos mocando, utilizamos o método
   // .mockImplementationOnce() do jest para mockar um retorno de uma função
-  test('Should return 500 if Email validator throws, jest mockImplementationOnce', () => {
+  test('Should return 500 if Email validator throws', () => {
     const {sut, emailValidatorStub} = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: "any_email@mail.com", 
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
+
+  test('Should return 500 if AddAccount throws', () => {
+    const {sut, addAccountStub} = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
       throw new Error()
     })
     const httpRequest = {
