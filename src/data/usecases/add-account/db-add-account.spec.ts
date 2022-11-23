1fae117fa15eb96115a6b573ae347f1278e7f82b
Issac Cabral <issaccabrall@gmail.com>
2022-11-23 10:22:47 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,16 +1,32 @@
 import { Encrypter } from '../../protocols/encrypter'
 import {DbAddAccount} from './db-add-account'
 
+interface SutTypes{
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter{
+    async encrypt(value: string): Promise<string>{
+      return 'hashed_password'
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeEncrypter()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
+
 describe('DbAddAccount UseCase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter{
-      async encrypt(value: string): Promise<string>{
-        return 'hashed_password'
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const {sut, encrypterStub} = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     /**
      * como é o controller que chama esse método, então name, email e password são campos
@@ -18,11 +34,24 @@ describe('DbAddAccount UseCase', () => {
      * essa variável accountData com os campos 'valid_name', 'valid_email' e 'valid_password' 
      */
     const accountData = {
-      name: 'valid_name',
+      name: 'valid_name', 
       email: 'valid_email',
       password: 'valid_password'
     }
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+
+  test('Should throw if Encrypter throws', async () => {
+    const {sut, encrypterStub} = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name', 
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
+
 })
