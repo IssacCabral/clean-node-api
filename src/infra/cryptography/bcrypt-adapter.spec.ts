2edf51614bea0d74c9cc858f37fc611323b69866
Issac Cabral <issaccabrall@gmail.com>
2022-11-23 15:36:19 -0300

    test: should return a hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -4,15 +4,29 @@ import { BcryptAdapter } from "./bcrypt-adapter"
 
 const salt = 12
 
+jest.mock('bcrypt', () => {
+  return {
+    async hash(): Promise<string>{
+      return 'hash'
+    }
+  }
+})
+
 const makeSut = (): Encrypter => {
   return new BcryptAdapter(salt)
 }
 
 describe('Bcrypt Adapter', () => {
-  test('Should call bcrypt with correct value', async () => {
+  test('Should call bcrypt with correct values', async () => {
     const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should return a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
