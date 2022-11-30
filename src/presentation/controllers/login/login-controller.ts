dad78495184f2d4e68de2072e97911fca1aa11a3
Issac Cabral <issaccabrall@gmail.com>
2022-11-30 13:41:19 -0300

    style: remove spaces

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -11,18 +11,22 @@ export class LoginController implements IController{
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try{
       const requiredFields = ["email", "password"]
+
       for(const field of requiredFields){
         if(!httpRequest.body[field]){
           return badRequest(new MissingParamError(field))
         }
       }
+      
       const {email, password} = httpRequest.body
       const isValid = this.emailValidator.isValid(email)
+
       if(!isValid){
         return badRequest(new InvalidParamError('email'))
       }
 
       const accessToken = await this.authentication.auth(email, password)
+
       if(!accessToken){
         return unauthorized()
       }
--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -121,8 +121,6 @@ describe('Login Controller', () => {
   test('Should return 200 if valid credentials is provided', async () => {
     const {sut} = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
-    expect(httpResponse).toEqual(ok({
-      accessToken: 'any_token'
-    }))
+    expect(httpResponse).toEqual(ok({accessToken: 'any_token'}))
   })
 })
